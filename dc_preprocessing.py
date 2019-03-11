ca2c6ab5a4be2d9c458d265857ad9e8bcf5dc804
anjanaw <shyameniw@gmail.com>
2019-03-11 13:24:14 +0000

    dc

--- dc_preprocessing.py
+++ dc_preprocessing.py
@@ -1,17 +1,20 @@
 import os
 import numpy as np
+import datetime as dt
 import matplotlib.pyplot as plt
 
-subject = '20'
-activities = ['01', '03', '04', '05', '06', '07']
+#subject = '03'
+subjects = ["03", "04", "05", "06", "07"]
+activities = ['01', '02', '03', '04', '05', '06', '07']
 
-folder = 'E:/Mex/Data/fullbackup/dc/'
-target_folder = 'E:/Mex/Data/1/dc/'
+folder = '/Volumes/1708903/MEx/Data/fullbackup/dc/'
+target_folder = '/Volumes/1708903/MEx/Data/1/dc/'
 
 data_file = 'this.bin'
 time_file = 'time.bin'
 full_file = '.csv'
 
+subjects_15_second_gap = ["03", "04", "05", "06", "07"]
 
 def write_data(file_path, data):
     if os.path.isfile(file_path):
@@ -23,40 +26,43 @@ def write_data(file_path, data):
     f.close()
 
 
-if not os.path.exists(target_folder+subject):
-    os.makedirs(target_folder+subject)
+for subject in subjects:
+    if not os.path.exists(target_folder+subject):
+        os.makedirs(target_folder+subject)
 
-for activity in activities:
-    print(activity)
-    fd = open(folder+subject+'_'+activity+'_1_'+data_file, "rb")
-    data = np.fromfile(fd, dtype=np.uint32)
-    ft = open(folder+subject+'_'+activity+'_1_'+time_file, "rb")
-    timeData = np.loadtxt(ft, dtype=np.unicode)
+    for activity in activities:
+        print(activity)
+        fd = open(folder+subject+'_'+activity+'_1_'+data_file, "rb")
+        data = np.fromfile(fd, dtype=np.uint32)
+        ft = open(folder+subject+'_'+activity+'_1_'+time_file, "rb")
+        timeData = np.loadtxt(ft, dtype=np.unicode)
 
-    #print(data.shape)
-    length = int(len(data)/(640*480))
-    data = np.reshape(data, (length,480,640))
-    data = data[:,:240,:320]
-    data = np.reshape(data, (length, 240*320))
-    #_data = np.reshape(data, (length*240*320))
-    #_data.sort()
-    #print(_data[0])
-    #print(str(_data[len(_data)-1])+','+str(_data[len(_data)-2])+','+str(_data[len(_data)-3]))
+        length = int(len(data)/(640*480))
+        data = np.reshape(data, (length,480,640))
+        data = data[:,:240,:320]
+        data = np.reshape(data, (length, 240*320))
+        #_data = np.reshape(data, (length*240*320))
+        #_data.sort()
+        #print(_data[0])
+        #print(str(_data[len(_data)-1])+','+str(_data[len(_data)-2])+','+str(_data[len(_data)-3]))
 
-    # minmax normalisation
-    _data = np.reshape(data, (length*240*320))
-    _data = _data / max(_data)
-    _data = np.reshape(_data, (length,240*320))
+        # minmax normalisation
+        _data = np.reshape(data, (length*240*320))
+        _data = _data / max(_data)
+        _data = np.reshape(_data, (length,240*320))
 
-    # write to pre-process step 1 folder
-    target_file = target_folder+subject+'/'+subject+'_'+activity+'.csv'
-    for itime, idata in zip(timeData, _data):
-        write_data(target_file, ' '.join(itime)+','+','.join([str(float("{0:.4f}".format(f))) for f in idata]))
+        # write to pre-process step 1 folder
+        target_file = target_folder+subject+'/'+subject+'_'+activity+'.csv'
+        for itime, idata in zip(timeData, _data):
+            tt = dt.datetime.strptime(' '.join(itime), '%Y-%m-%d %H:%M:%S.%f')
+            if subject in subjects_15_second_gap:
+                tt = tt + dt.timedelta(seconds=-15)
+            write_data(target_file, str(tt)+','+','.join([str(float("{0:.4f}".format(f))) for f in idata]))
 
-    # visualise
-    #for i in range(len(_data)):
-    #    if i % 2000:
-    #        plt.imshow(_data[i])
-    #        plt.show()
+        # visualise
+        #for i in range(len(_data)):
+        #    if i % 2000:
+        #        plt.imshow(_data[i])
+        #        plt.show()
 
 
